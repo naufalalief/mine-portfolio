be9d2ac579d226c9fdcc5db276f455f5c582eb18
Naufal Alief <naufalalief087@gmail.com>
2023-09-29 00:04:07 +0700

    edit darkmode

--- js/script.js
+++ js/script.js
@@ -61,10 +61,22 @@
 const toggleBtn = document.getElementById("toggle-btn");
 const icon = toggleBtn.querySelector("img");
 
+// Cek apakah ada status terakhir pada localStorage
+const isDarkMode = localStorage.getItem("isDarkMode") === "true";
+if (isDarkMode) {
+  document.body.classList.add("dark");
+  icon.src = "image/sun.svg";
+} else {
+  document.body.classList.remove("dark");
+  icon.src = "image/moon.svg";
+}
+
 toggleBtn.addEventListener("click", function () {
   document.body.classList.toggle("dark");
   const isDarkMode = document.body.classList.contains("dark");
   icon.src = isDarkMode ? "image/sun.svg" : "image/moon.svg";
+
+  localStorage.setItem("isDarkMode", isDarkMode);
 });
 
 // const bilPrima = (n) => {
