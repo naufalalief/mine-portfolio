f64cfc70f40ff543952544b529898b72e42be050
Naufal Alief <naufalalief087@gmail.com>
2023-09-30 21:40:50 +0700

    done

--- js/script.js
+++ js/script.js
@@ -17,22 +17,3 @@ toggleBtn.addEventListener("click", function () {
 
   localStorage.setItem("isDarkMode", isDarkMode);
 });
-
-const toggleBtn2 = document.getElementById("toggle-btn");
-const icon2 = toggleBtn.querySelector("img");
-
-if (isDarkMode) {
-  document.body.classList.add("dark");
-  icon.src = "../image/sun.svg";
-} else {
-  document.body.classList.remove("dark");
-  icon.src = "../image/moon.svg";
-}
-
-toggleBtn.addEventListener("click", function () {
-  document.body.classList.toggle("dark");
-  const isDarkMode = document.body.classList.contains("dark");
-  icon.src = isDarkMode ? "../image/sun.svg" : "../image/moon.svg";
-
-  localStorage.setItem("isDarkMode", isDarkMode);
-});
