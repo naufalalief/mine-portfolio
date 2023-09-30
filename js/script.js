9f52b0df556c48fa04ebc7a6cfa6cfb706bbf27c
Naufal Alief <naufalalief087@gmail.com>
2023-09-30 21:14:50 +0700

    done?

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ const icon = toggleBtn.querySelector("img");
 const isDarkMode = localStorage.getItem("isDarkMode") === "true";
 if (isDarkMode) {
   document.body.classList.add("dark");
-  icon.src = "../image/sun.svg";
+  icon.src = "image/sun.svg";
 } else {
   document.body.classList.remove("dark");
-  icon.src = "../image/moon.svg";
+  icon.src = "image/moon.svg";
 }
 
 toggleBtn.addEventListener("click", function () {
   document.body.classList.toggle("dark");
   const isDarkMode = document.body.classList.contains("dark");
-  icon.src = isDarkMode ? "../image/sun.svg" : "../image/moon.svg";
+  icon.src = isDarkMode ? "image/sun.svg" : "image/moon.svg";
 
   localStorage.setItem("isDarkMode", isDarkMode);
 });
