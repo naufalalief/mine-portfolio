d4f532ee7e209143fb5db5b16defcab72dff4e40
Naufal Alief <naufalalief087@gmail.com>
2023-09-30 22:09:38 +0700

    done

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@ const toggleBtn = document.getElementById("toggle-btn");
 const icon = toggleBtn.querySelector("img");
 
 const isDarkMode = localStorage.getItem("isDarkMode") === "true";
+
+function setDarkMode(isDark) {
+  document.body.classList.toggle("dark", isDark);
+  icon.src = isDark ? "image/sun.svg" : "image/moon.svg";
+  localStorage.setItem("isDarkMode", isDark);
+}
+
 if (isDarkMode) {
-  document.body.classList.add("dark");
-  icon.src = "image/sun.svg";
-} else {
-  document.body.classList.remove("dark");
-  icon.src = "image/moon.svg";
+  setDarkMode(true);
 }
 
 toggleBtn.addEventListener("click", function () {
-  document.body.classList.toggle("dark");
-  const isDarkMode = document.body.classList.contains("dark");
-  icon.src = isDarkMode ? "image/sun.svg" : "image/moon.svg";
-
-  localStorage.setItem("isDarkMode", isDarkMode);
+  const isDarkMode = !document.body.classList.contains("dark");
+  setDarkMode(isDarkMode);
 });
